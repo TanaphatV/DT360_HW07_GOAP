2c750194f21910c37e591bb3f5e40310ed2eb8b9
Tanaphat Vivattanakijcharoen <tanaphatviva@gmail.com>
2023-05-25 12:15:38 +0700

    Initial commit

---
+++ Assets/Scripts/Register.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Register : GAction
+{
+    // Start is called before the first frame update
+    public override bool PrePerform()
+    {
+        return true;
+    }
+
+    public override bool PostPerform()
+    {
+        return true;
+    }
+}
---
+++ Assets/Scripts/WaitForNurse.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaitForNurse : GAction
+{
+    public override bool PrePerform()
+    {
+
+        return true;
+    }
+
+    public override bool PostPerform()
+    {
+        GWorld.Instance.GetWorld().ModifyState("Waiting", 1);
+        GWorld.Instance.AddPatient(gameObject);
+
+        beliefs.ModifyState("atHospital", 1);
+        return true;
+    }
+}
